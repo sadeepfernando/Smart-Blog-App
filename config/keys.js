eece5e5e1305591423a9dd519db0ecc5c2a4a24d
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-05-31 12:23:17 +0530

    AWS S3 setup

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 
-const {PORT, CONNECTION_URL, JWT_SECRET,SENDER_EMAIL,EMAIL_PASSWORD} = process.env;
+const {PORT, CONNECTION_URL, JWT_SECRET,SENDER_EMAIL,EMAIL_PASSWORD,AWS_ACCESS_KEY,AWS_SECRET_ACCESS_KEY,AWS_BUCKET_NAME,AWS_REGION} = process.env;
 
 
 module.exports = 
@@ -8,6 +8,10 @@ module.exports =
     connection_url:CONNECTION_URL,
      jwtSecret:JWT_SECRET,
      senderEmail:SENDER_EMAIL,
-     emailPassword:EMAIL_PASSWORD
+     emailPassword:EMAIL_PASSWORD,
+     awsAccessKey:AWS_ACCESS_KEY,
+     awsSecretAccessKey:AWS_SECRET_ACCESS_KEY,
+     awsbucketName:AWS_BUCKET_NAME,
+     awsRegion:AWS_REGION
 
 };
