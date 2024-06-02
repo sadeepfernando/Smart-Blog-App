9ba25ba7eb3ef1140376cb691aec2ff4eaf7d51e
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-06-02 15:50:44 +0530

    craeted the add post controller

--- controllers/post.js
+++ controllers/post.js
@@ -1,9 +1,42 @@
-
+const { File , Category , Post } = require('../models/index')
 
 
 const addPost = async(req, res, next) =>{
     try {
-        
+        const { title, desc, file, category } = req.body;
+        const {_id} = req.user;
+
+        //Check wheather file is exist or not
+        if(file){
+            const isFileExist = await File.findById(file);
+
+            if(!isFileExist){
+                res.code = 404;
+                throw new Error('File is required');
+            }
+        }
+
+        //Check wheather the category exists
+        const isCategoryExist = await Category.findbyId(category);
+
+        if(!isCategoryExist){
+            res.code = 404;
+            throw new Error('Category is required');
+        }
+
+        const newPost = new Post({
+            title, 
+            desc,
+            file,
+            category,
+            updatedBy: _id,
+        });
+
+        await newPost.save();
+
+        res.status(201)
+        .json({code:201, status:true, message:'Post added successfully'});
+
     } catch (error) {
         next(error);
     }
