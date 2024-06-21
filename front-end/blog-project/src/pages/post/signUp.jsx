a95d48dcbc5a11220367ff55e7c7a733d809f2d1
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-06-21 09:39:00 +0530

    create state handling using react hooks in the form

--- front-end/blog-project/src/pages/post/signUp.jsx
+++ front-end/blog-project/src/pages/post/signUp.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import { useState } from "react";
+
+const initialFormData = {
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const initialFormError = {
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 
 export default function signUp() {
+  const [formData, setFormData] = useState(initialFormData);
+
+  //for error handling
+  const [formError, setFormError] = useState(initialFormError);
+
+  //to handle events
+  const handleChange = (e) => {
+    setFormData((prev) =>({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  //handle form submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <div className="form-container">
-      <form action="" className="inner-container">
+      <form className="inner-container" onSubmit={handleSubmit}>
         <h2 className="form-title">SignUp Form</h2>
 
         <div className="form-group">
@@ -13,6 +44,8 @@ export default function signUp() {
             name="name"
             placeholder="Sadeep Fernando"
             className="form-control"
+            value={formData.name}
+            onChange={handleChange}
           />
         </div>
 
@@ -23,6 +56,8 @@ export default function signUp() {
             name="email"
             placeholder="SadeepFernando@gmail.com"
             className="form-control"
+            value={formData.email}
+            onChange={handleChange}
           />
         </div>
 
@@ -33,6 +68,8 @@ export default function signUp() {
             name="password"
             placeholder="*********"
             className="form-control"
+            value={formData.password}
+            onChange={handleChange}
           />
         </div>
 
@@ -43,17 +80,14 @@ export default function signUp() {
             name="confirmPassword"
             placeholder="*********"
             className="form-control"
+            value={formData.confirmPassword}
+            onChange={handleChange}
           />
         </div>
 
         <div className="form-group">
-          <input
-            type="submit"
-            className="button"
-            value="Signup"
-          />
+          <input type="submit" className="button" value="Signup" />
         </div>
-
       </form>
     </div>
   );
