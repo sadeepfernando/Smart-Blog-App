d454ed11ca30b052229147d448efd73b62f0b166
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-06-21 09:05:46 +0530

    created signup form

--- front-end/blog-project/src/pages/post/signUp.jsx
+++ front-end/blog-project/src/pages/post/signUp.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
 
 export default function signUp() {
   return (
-    <div>
-      This is signup page
+    <div className="form-container">
+      <form action="" className="inner-container">
+        <h2 className="form-title">SignUp Form</h2>
+
+        <div className="form-group">
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Sadeep Fernando"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="SadeepFernando@gmail.com"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="*********"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Confirm Password</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            placeholder="*********"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            type="submit"
+            className="button"
+            value="Signup"
+          />
+        </div>
+
+      </form>
     </div>
-  )
+  );
 }
