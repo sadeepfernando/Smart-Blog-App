29c88ba57450f00e09759591bf668dc9d2062df9
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-06-28 13:31:54 +0530

    created the post list page basic design

--- front-end/blog-project/src/pages/post/postList.jsx
+++ front-end/blog-project/src/pages/post/postList.jsx
@@ -1,9 +1,86 @@
 import React from 'react'
+import cardImg from '../../assets/image/place.jpeg';
 
 export default function postList() {
   return (
     <div>
-      This is ppst list page
+      <button className='button button-block'>Add New Post</button>
+      <h2 className='table-title'>Post List</h2>
+
+      <input type="text" className='search-input' name='search' placeholder='Search here' />
+
+      <div className='flexbox-container wrap'>
+
+        <div className="post-card">
+          <h4 className='card-title'>React Blog project</h4>
+          <p className='card-desc'>This is the description of react blog project</p>
+          <img src={cardImg} alt="mern" className='card-img' />
+        </div>
+
+        <div className="post-card">
+          <h4 className='card-title'>React Blog project</h4>
+          <p className='card-desc'>This is the description of react blog project</p>
+          <img src={cardImg} alt="mern" className='card-img' />
+        </div>
+
+        <div className="post-card">
+          <h4 className='card-title'>React Blog project</h4>
+          <p className='card-desc'>This is the description of react blog project</p>
+          <img src={cardImg} alt="mern" className='card-img' />
+        </div>
+
+        <div className="post-card">
+          <h4 className='card-title'>React Blog project</h4>
+          <p className='card-desc'>This is the description of react blog project</p>
+          <img src={cardImg} alt="mern" className='card-img' />
+        </div>
+
+        <div className="post-card">
+          <h4 className='card-title'>React Blog project</h4>
+          <p className='card-desc'>This is the description of react blog project</p>
+          <img src={cardImg} alt="mern" className='card-img' />
+        </div>
+
+        <div className="post-card">
+          <h4 className='card-title'>React Blog project</h4>
+          <p className='card-desc'>This is the description of react blog project</p>
+          <img src={cardImg} alt="mern" className='card-img' />
+        </div>
+
+        <div className="post-card">
+          <h4 className='card-title'>React Blog project</h4>
+          <p className='card-desc'>This is the description of react blog project</p>
+          <img src={cardImg} alt="mern" className='card-img' />
+        </div>
+
+        <div className="post-card">
+          <h4 className='card-title'>React Blog project</h4>
+          <p className='card-desc'>This is the description of react blog project</p>
+          <img src={cardImg} alt="mern" className='card-img' />
+        </div>
+
+        <div className="post-card">
+          <h4 className='card-title'>React Blog project</h4>
+          <p className='card-desc'>This is the description of react blog project</p>
+          <img src={cardImg} alt="mern" className='card-img' />
+        </div>
+
+        <div className="post-card">
+          <h4 className='card-title'>React Blog project</h4>
+          <p className='card-desc'>This is the description of react blog project</p>
+          <img src={cardImg} alt="mern" className='card-img' />
+        </div>
+
+
+
+
+
+
+
+
+
+
+      </div>
     </div>
   )
 }
