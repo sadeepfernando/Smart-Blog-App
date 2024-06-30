57e91625726dda426c0fb9069eef0e2ee6c56957
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-06-30 13:25:52 +0530

    created the update post page

--- front-end/blog-project/src/pages/post/updatePost.jsx
+++ front-end/blog-project/src/pages/post/updatePost.jsx
@@ -3,7 +3,42 @@ import React from 'react'
 export default function updatePost() {
   return (
     <div>
-      This is update post page
+      <button className='button button-block'>Go Back</button>
+
+      <div className="form-container">
+        <form className="innner-container">
+          <h2 className="form-title">Update Post</h2>
+
+          <div className="form-group">
+            <label>Title</label>
+            <input type="text" name="title" className='form-control' placeholder='React Blog Project' />
+          </div>
+
+          <div className="form-group">
+            <label>Description</label>
+            <textarea name="desc" className='form-control' placeholder='This is the description of react blog project'></textarea>
+          </div>
+
+          <div className="form-group">
+            <label>Select an Image</label>
+            <input type="file" name="file" className='form-control' />
+          </div>
+
+          <div className="form-group">
+            <label>Category</label>
+            <select className='form-control'>
+              <option value="category 1">Category 1</option>
+              <option value="category 2">Category 2</option>
+              <option value="category 3">Category 3</option>
+            </select>
+          </div>
+
+          <div className="form-group">
+            <input type="submit" className='button' value='Add'  />
+          </div>
+
+        </form>
+      </div>
     </div>
   )
 }
